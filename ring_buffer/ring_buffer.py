e2f6edb18a1c6852b37c2263c1e9068a0ddcb054
dylan0stewart <57077858+dylan0stewart@users.noreply.github.com>
2020-07-17 11:42:22 -0400

    completed ring_buffer
    completed part 1 of sprint

--- ring_buffer/ring_buffer.py
+++ ring_buffer/ring_buffer.py
@@ -1,9 +1,51 @@
+ 
 class RingBuffer:
-    def __init__(self, capacity):
-        pass
+  def __init__(self, capacity):
+    '''
+    initialize with base values needed
+    '''
+    self.capacity = capacity
+    self.current = 0
+    self.storage = [None]*capacity
+    self.rear = -1
+    self.front = -1
 
-    def append(self, item):
-        pass
+  def append(self, item):
+   if self.current < self.capacity: #if its less
+      oldest = self.storage.pop(self.current) #instantiate oldest
+      self.storage.insert(self.current, item) #insert new
+      print("After: ", self.storage, "Removed Item: ", oldest)#print changes
+      self.current = self.current + 1 #add 1 every time to make sure it resets the loop below
 
-    def get(self):
-        pass
+      if self.current == self.capacity: #when current gets to capacity
+        self.current = 0 #reset current to 0
+
+
+  def get(self):
+    templist = [] #creates empty list
+    for item in self.storage: #iterates through storage
+      if item != None: #if item exists
+        templist.append(item) #append the item
+    return templist #return list when done iterating
+
+
+'''Copied over mini-test to check as i go'''
+# buffer = RingBuffer(3)
+
+# buffer.get()   # should return []
+
+# buffer.append('a')
+# buffer.append('b')
+# buffer.append('c')
+
+# buffer.get()   # should return ['a', 'b', 'c']
+
+# # 'd' overwrites the oldest value in the ring buffer, which is 'a'
+# buffer.append('d')
+
+# buffer.get()   # should return ['d', 'b', 'c']
+
+# buffer.append('e')
+# buffer.append('f')
+
+# buffer.get()   # should return ['d', 'e', 'f']
